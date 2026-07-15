1d95f8366bee97b9559f9553f860d35b1fb8c360
Stephen Po-Chedley <pochedley@gmail.com>
2026-07-15 15:13:26 -0700

    Add support for kerchunk

--- xsearch/__init__.py
+++ xsearch/__init__.py
@@ -13,7 +13,7 @@ from .search import addAttribute
 import os
 
 # version
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 # since this software is installed centrally and may be updated
 # this section of code stores the xsearch version in a hidden
